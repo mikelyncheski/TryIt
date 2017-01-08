3e75d789317ea964e2c39b3b4def21df2484cb69
Mike Lyncheski <mlyncheski@users.noreply.github.com>
2017-01-08 08:18:00 -0500

    More

--- YouDontKnowJS/Book1/Chapter2-TheBasics.js
+++ YouDontKnowJS/Book1/Chapter2-TheBasics.js
@@ -148,3 +148,31 @@ var x = function bar(){
 })();
 // "Hello!"
 
+
+// Closure
+// You can think of closure as a way to "remember" and continue to access a function's scope (its variables) even once the function has finished running.
+
+function makeAdder(x) {
+    // parameter `x` is an inner variable
+
+    // inner function `add()` uses `x`, so
+    // it has a "closure" over it
+    function add(y) {
+        return y + x;
+    };
+
+    return add;
+}
+// `plusOne` gets a reference to the inner `add(..)`
+// function with closure over the `x` parameter of
+// the outer `makeAdder(..)`
+var plusOne = makeAdder( 1 );
+
+// `plusTen` gets a reference to the inner `add(..)`
+// function with closure over the `x` parameter of
+// the outer `makeAdder(..)`
+var plusTen = makeAdder( 10 );
+
+console.log(plusOne( 3 ));       // 4  <-- 1 + 3
+console.log(plusOne( 41) );      // 42 <-- 1 + 41
+console.log(plusTen( 13));      // 23 <-- 10 + 13
